9491d3f17da05d4bebd2c527d450358a6a8cca0c
paulmlaviwa <mlaviwa.paul@gmail.com>
2023-12-21 11:49:51 -0500

    Updated Products controller

--- src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductsController.java
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductsController.java
@@ -4,45 +4,26 @@ import com.pluralsight.NorthwindTradersAPI.models.Product;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
+import com.pluralsight.NorthwindTradersAPI.dao.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @RestController
+@RequestMapping("/products")
 public class ProductsController {
 
-    private final List<Product> products = Arrays.asList(
-            new Product(1, "Chicken Gizzard", 1, 2.59),
-            new Product(2, "Tylenol", 2, 6.99)
-    );
+    private final ProductDao productDao;
 
-    @RequestMapping("/products")
-    public List<Product> getAllProducts(
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) Integer categoryId,
-            @RequestParam(required = false) Double maxPrice) {
+    @Autowired
+    public ProductsController(ProductDao productDao) {
+        this.productDao = productDao;
+    }
 
-        List<Product> filteredProducts = products;
-
-        if (name != null) {
-            filteredProducts = filteredProducts.stream()
-                    .filter(product -> product.getProductName().contains(name))
-                    .collect(Collectors.toList());
-        }
-
-        if (categoryId != null) {
-            filteredProducts = filteredProducts.stream()
-                    .filter(product -> product.getCategoryId() == categoryId)
-                    .collect(Collectors.toList());
-        }
-
-        if (maxPrice != null) {
-            filteredProducts = filteredProducts.stream()
-                    .filter(product -> product.getUnitPrice() <= maxPrice)
-                    .collect(Collectors.toList());
-        }
-
-        return filteredProducts;
+    @RequestMapping
+    public List<Product> getAllProducts(@RequestParam(required = false) String name,
+                                        @RequestParam(required = false) Integer categoryId,
+                                        @RequestParam(required = false) Double maxPrice) {
+        return productDao.getAll();
     }
 }
